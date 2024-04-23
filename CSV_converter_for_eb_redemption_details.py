20e592e2dc88b2613d12792feaf728de50eab370
23110350 <156343159+23110350@users.noreply.github.com>
2024-04-23 23:21:11 +0530

    Add files via upload
    Make a Folder. Let it be X.
    Inside the "X" folder, create the following subfolders:
      1. templates: For HTML templates. Inside thi:
           a. index.html
           b. search_results.html
    Inside the "X" folder, create the following file:
      1. app.py: This will contain your Flask application code.

---
+++ CSV_converter_for_eb_redemption_details.py
@@ -0,0 +1,33 @@
+!pip install tabula-py pandas
+import tabula
+import pandas as pd
+import numpy as np
+
+def clean_data(df):
+    df = df.dropna(how='all')
+    df.columns = df.columns.str.strip()
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    df = df.replace('', np.nan)
+    df = df.dropna()
+    return df
+
+def pdf_to_array(pdf_path):
+    tables = tabula.read_pdf(pdf_path, pages='all', multiple_tables=True)
+    df = pd.concat(tables)
+    df = clean_data(df)
+    data_array = df.values
+    return data_array
+
+pdf_path = 'C:\Users\sony\Desktop\DCC_App\PDF Files\EB_Redemption_Details.pdf'
+data_array = pdf_to_array(pdf_path)
+
+print(data_array)
+columns = ['Sr No.', 'Date of Encashment', 'Name of the Political Party',
+           'Account no. of Political Party', 'Prefix', 'Bond Number',
+           'Denominations', 'Pay Branch Code', 'Pay Teller']
+
+df1 = pd.DataFrame(data_array, columns=columns)
+
+df1['Pay Branch Code'] = df1['Pay Branch Code'].apply(lambda x: str(x).zfill(5))
+df1['Pay Branch Code'] = df1['Pay Branch Code'].apply(lambda x: str(x))
+df1.to_csv('EB_Redemption_Details.csv', index=False)
